00bd39d775515573da33de24abd8bc860c708b9c
Diya S Reddy <diyasr0105@gmail.com>
2025-09-06 17:22:36 +0530

    Update views.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product  # make sure Product model exists in models.py
 
-# Create your views here.
+# Homepage view
+def home(request):
+    return render(request, 'marketplace/home.html')
+
+# Product listing view
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'marketplace/product_list.html', {'products': products})
+
+# Product detail view
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'marketplace/product_detail.html', {'product': product})
 
